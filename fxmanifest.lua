5120a4ed79f4ddd9df0a763d22fcb90ec57af2ec
soukyotaka <gaarayt007@gmail.com>
2023-05-21 02:17:14 -0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
 
-Author { 'Kyotaka#0007' }
-Discord { 'https://discord.gg/RVPcK7sQAK' }
+Author 'Kyotaka#0007'
+Discord 'https://discord.gg/RVPcK7sQAK'
+version '2.0.0'
 
 shared_scripts {
    'shared.lua'
